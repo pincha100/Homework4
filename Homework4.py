d1d4c5eed99a18783aeeda67eaef04033a6636fd
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-07-15 18:22:42 +0700

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,6 @@
+my_string = input("Введите ФИО ")
+print("Приветсвую ", my_string .upper())
+print("Приветсвую ", my_string .lower())
+print("Приветсвую ", my_string .replace(" ", "" ))
+print("Приветсвую ", my_string [0])
+print("Приветсвую ", my_string [-1])
